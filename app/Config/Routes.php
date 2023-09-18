72043c9652952322df27d0a4370d8c8d765c93bf
MRadityaAdhirajasa <radityaadhirajasaa@gmail.com>
2023-09-18 21:07:50 +0700

    prak 2

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -1,8 +1,12 @@
 <?php
 
+use App\Controllers\UserController;
 use CodeIgniter\Router\RouteCollection;
 
 /**
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->get('/profile/(:any)/(:any)/(:any)', 'Home::profile/$1/$2/$3');
+
+$routes->get('/profile', 'Home::profile');
